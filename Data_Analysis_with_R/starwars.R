591955386950401ae527ec4f6f9fcf1a54ea1d08
Oludare Stephen <oludarestephen00@gmail.com>
2022-02-02 05:22:45 +0100

    Working on data cleaning

--- Data_Analysis_with_R/starwars.R
+++ Data_Analysis_with_R/starwars.R
@@ -1,7 +1,7 @@
-#installing pachakges
+#installing packages
 install.packages("tidyverse")
 
-#loading my starwars dataset from tidyverse
+#loading my starwars data set from tidyverse
 library(tidyverse)
 view(starwars)
 
@@ -10,5 +10,18 @@ glimpse(starwars)
 class(starwars)
 unique(starwars$gender)
 
-#since gender is a categotical variable, let's change it from char to factor
+#since gender is a categorical variable, let's change it from char to factor
+starwars$gender <- as.factor(starwars$gender)
+class(starwars$gender)
 
+#checking the gender levels
+levels(starwars$gender)
+
+#changing the levels of the gender
+starwars$gender <- factor((starwars$gender), levels = c("masculine","feminine"))
+levels((starwars$gender))
+
+#now let us do some filtering
+starwars %>% 
+  select(name, height, ends_with("color")) %>% filter(hair_color %in% c("blond","brown") & height < 180)
+  
