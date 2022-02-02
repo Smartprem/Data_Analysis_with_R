d4a418b269eb8b7a940f432ea9b8007b853d768f
Oludare Stephen <oludarestephen00@gmail.com>
2022-02-02 10:07:00 +0100

    filtering and missing values

--- Data_Analysis_with_R/starwars.R
+++ Data_Analysis_with_R/starwars.R
@@ -25,3 +25,20 @@ levels((starwars$gender))
 starwars %>% 
   select(name, height, ends_with("color")) %>% filter(hair_color %in% c("blond","brown") & height < 180)
   
+#Now let's deal with our missing data
+#we calc the mean of the height without the missing value
+mean(starwars$height, na.rm = TRUE)
+
+#we could just omit the missing values using na.omit() but that is not recommended because we might lose very important data we need
+#we can try to understand where our value is missing by filtering out where we have na
+starwars %>%
+  select(name, gender, hair_color, height) %>% 
+  filter(!complete.cases(.))%>%
+  drop_na(height)
+
+#we want to replace na with 'none' in our hair colour
+starwars %>%
+  select(name, gender, hair_color, height) %>% 
+  filter(!complete.cases(.))%>%
+  mutate(hair_color = replace_na(hair_color, "none"))
+  
